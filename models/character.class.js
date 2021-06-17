cd1c24771de9e242300e423de827ffd1839c0a31
Seweryn Korzekwa <seweryn.korzekwa@outlook.de>
2021-06-17 17:25:45 +0200

    3-03

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObjects {
 
     height = 280;
-    y = 155;
+    y = 55;
     speed = 3;
     IMG_WALKING = [
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png',
@@ -9,23 +9,36 @@ class Character extends MovableObjects {
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-23.png',
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-24.png',
         'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-25.png',
-        'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-26.png'
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-26.png',
+    ];
+    IMG_JUMPING = [
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-31.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-32.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-33.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-34.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-35.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-36.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-37.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-38.png',
+        'img/2.Secuencias_Personaje-Pepe-correcciขn/3.Secuencia_salto/J-39.png'
     ];
     world;
 
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png');
         this.loadImages(this.IMG_WALKING);
+        this.loadImages(this.IMG_JUMPING);
         this.animate();
+        this.applyGravity();
     }
 
     animate() {
         setInterval(() => {
-            if(this.world.keyboard.RIGHT && this.x < this.world.level.levelEndX) {
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.levelEndX) {
                 this.otherDirection = false;
                 this.x += this.speed;
             }
-            if(this.world.keyboard.LEFT && this.x > 120) {
+            if (this.world.keyboard.LEFT && this.x > 120) {
                 this.otherDirection = true
                 this.x -= this.speed;
             }
@@ -33,8 +46,12 @@ class Character extends MovableObjects {
         }, 1000 / 60);
 
         setInterval(() => {
-            if (this.world.keyboard.LEFT || this.world.keyboard.RIGHT) {
-                this.playAnimation(this.IMG_WALKING)
+            if (this.isAboveGrund()) {
+                this.playAnimation(this.IMG_JUMPING)
+            } else {
+                if (this.world.keyboard.LEFT || this.world.keyboard.RIGHT) {
+                    this.playAnimation(this.IMG_WALKING)
+                }
             }
         }, 1000 / 8);
     }
