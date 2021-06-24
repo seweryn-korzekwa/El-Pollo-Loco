406340ea884dc05dd40ea7f8f84b90d25dd81eb1
Seweryn Korzekwa <seweryn.korzekwa@outlook.de>
2021-06-24 16:11:26 +0200

    3-17

--- models/status-bar-class.js
+++ models/status-bar-class.js
@@ -1,3 +1,38 @@
-class StatusBar {
-    
+class StatusBar extends DrawableObject {
+    percentage = 100;
+
+    IMAGES = [
+        'img/7.Marcadores/Barra/Marcador vida/azul/0_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/20_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/40_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/60_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/80_.png',
+        'img/7.Marcadores/Barra/Marcador vida/azul/100_.png',
+    ]
+
+    constructor() {
+        this.loadImages(this.IMAGES);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()]
+        this.imageCache[path] = img;
+    }
+
+    resolveImageIndex() {
+        if (this.percentage === 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 1;
+        }
+    }
 }
